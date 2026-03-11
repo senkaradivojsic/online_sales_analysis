bf596b28abb3dc4d45aa870da8c3423d7e8c48cc
Senka Radivojsic <shadowshadow9218@gmail.com>
2026-03-11 13:30:18 +0100

    Izmene u datoteci main.py

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
-from product import Product
 from product_manager import ProductManager
 from cart import Cart
 
 manager = ProductManager()
 cart = Cart()
 
-manager.add_product("Laptop", 86000, 5)
-manager.add_product("Smartphone", 50000, 10)
-manager.add_product("Headphones", 15000, 20)
-manager.add_product("Monitor", 30000, 7)
-
-for product in manager.get_products():
-    print(product)
-
-total = manager.total_amount()
-print(f"Total amount of all products: {total}")
+manager.add_product("Laptop", 86000, 3)
+manager.add_product("Smartphone", 50000, 5)
+manager.add_product("Headphones", 15000, 10)
+manager.add_product("Monitor", 30000, 6)
 manager.remove_product("Smartphone")
 
 for product in manager.get_products():
